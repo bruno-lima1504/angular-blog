ed30e4eea90044dc1ebf971d7f078c674be5ffe9
Bruno Lima <bruno.lima1504@gmail.com>
2022-11-23 17:57:15 -0300

    finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover":"https://cloud.estacaonerd.com/wp-content/uploads/2019/03/05220821/logo-marvel-vilains.jpg",
+        "title":"Novo jogo da Marvel anunciado!",
+        "description": "Marvel anuncia novo Jogo para todas as plataformas! Os gráficos estão incriveis, e o jogo traz opção multiplataforma."
+    },
+    {
+        "id": "2",
+        "photoCover":"https://super.abril.com.br/wp-content/uploads/2003/07/marvel-fb.png",
+        "title":"Nova serie anunciada no Disney+",
+        "description": "Nova serie anunciada no Disney+, vem ai uma série exclusiva dos vingadores!!"
+    },
+    {
+        "id": "3",
+        "photoCover":"https://br.hedgehogdigital.co.uk/wp-content/uploads/2021/11/HHG-BLOG-HOMEM-ARANHA-PANTERA-NEGRA-1024x682.png",
+        "title":"Filme do Pantera Negra com o o Homem Aranha, estréia em breve!",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eaque voluptatem deleniti asperiores excepturi distinctio voluptatibus iure ex, natus provident quam assumenda quia consequatur placeat ipsam sequi laborum dolor quasi."
+    },
+    {
+        "id": "4",
+        "photoCover":"https://cdn.pocket-lint.com/r/s/x1920/assets/images/149551-tv-feature-what-is-the-best-order-to-watch-the-x-men-movies-image1-y5wpzep24w.jpg",
+        "title":"Filmagem de novo X-man, foi iniciado em 30 de fevereiro!",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eaque voluptatem deleniti asperiores excepturi distinctio voluptatibus iure ex, natus provident quam assumenda quia consequatur placeat ipsam sequi laborum dolor quasi."
+    }
+]
